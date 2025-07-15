10c7af58cc34bb8ecfd03d38369a6dc2ede35820
M'barek Benraiss <81750067+MbarekDev-Lab@users.noreply.github.com>
2025-07-15 22:02:13 +0200

    //JPA/Hibernate > database REST API.

--- microservices/src/main/java/com/rest_api_webservices/microservices/controller/HelloWorldController.java
+++ microservices/src/main/java/com/rest_api_webservices/microservices/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.rest_api_webservices.microservices.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,11 +12,13 @@ public class HelloWorldController {
     public String helloWorld() {
         return "Hello, world!";
     }
-
-
-
     @GetMapping("/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello from Bean!");
+    }
+
+    @GetMapping("/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello from!, %s", name));
     }
 }
---
+++ microservices/src/main/java/com/rest_api_webservices/microservices/dao/UserDaoService.java
@@ -0,0 +1,41 @@
+package com.rest_api_webservices.microservices.dao;
+
+import com.rest_api_webservices.microservices.user.User;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class UserDaoService {
+
+    // Simulated in-memory database
+    private static final List<User> users = new ArrayList<>();
+    private static int userCount = 0;
+
+    static {
+        users.add(new User(++userCount, "Adam", LocalDate.now().minusYears(30)));
+        users.add(new User(++userCount, "Eve", LocalDate.now().minusYears(25)));
+        users.add(new User(++userCount, "Jack", LocalDate.now().minusYears(20)));
+    }
+
+    public List<User> findAll() {
+        return users;
+    }
+
+    public User save(User user) {
+        if (user.getId() == null) {
+            user.setId(++userCount);
+        }
+        users.add(user);
+        return user;
+    }
+
+    public User findOne(int id) {
+        return users.stream()
+                .filter(user -> user.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
+}
