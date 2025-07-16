1730ed8554977faf8394d9d2ba6584c913fb0307
M'barek Benraiss <81750067+MbarekDev-Lab@users.noreply.github.com>
2025-07-16 08:34:56 +0200

    Added basic REST API for User resource with in-memory data storage

--- microservices/src/main/java/com/rest_api_webservices/microservices/resources/UserResource.java
+++ microservices/src/main/java/com/rest_api_webservices/microservices/resources/UserResource.java
@@ -2,9 +2,13 @@ package com.rest_api_webservices.microservices.resources;
 
 import com.rest_api_webservices.microservices.dao.UserDaoService;
 import com.rest_api_webservices.microservices.user.User;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -26,11 +30,36 @@ public class UserResource {
     public User retrieveFirstUsers(@PathVariable int id){
         return service.findOne(id);
     }
-
-    // POST / users
-    @PostMapping("/userts")
-    public void createUser(@RequestBody User user){
+    @PostMapping("/creatusers")
+    public  ResponseEntity<User> createUser1(@RequestBody User user){
         service.save(user);
+        return ResponseEntity.created(null).build();
     }
+
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+
+
+//    // POST /users
+//    @PostMapping("/users")
+//    public ResponseEntity<User> createUser(@RequestBody User user) {
+//        User savedUser = service.save(user);
+//        // Build URI to /users/{id}
+//        URI location = URI.create(String.format("/users/%d", savedUser.getId()));
+//        return ResponseEntity.created(location).body(savedUser);
+//    }
+
 
 }
