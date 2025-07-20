aced6b3ab0a77c20da6a73744264a358c04893ba
M'barek Benraiss <81750067+MbarekDev-Lab@users.noreply.github.com>
2025-07-20 16:47:57 +0200

    Enhancing REST API to connect to H" useing JPA and Hibernate

--- microservices/src/main/java/com/rest_api_webservices/microservices/resources/UserJpaResource.java
+++ microservices/src/main/java/com/rest_api_webservices/microservices/resources/UserJpaResource.java
@@ -19,69 +19,47 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
+@RequestMapping("/jpa")
 public class UserJpaResource {
-    // no more using UserDaoService
-    //private final UserDaoService service;
     private final UserRepository userRepository;
 
-    public UserJpaResource(UserDaoService service, UserRepository userRepository){
-        //this.service=service;
+    public UserJpaResource(UserRepository userRepository){
         this.userRepository = userRepository;
     }
 
-    //GET /users
-    @GetMapping("/jpa/users")
+    @GetMapping("/users")
     public List<User> retrieveAllUsers(){
         return userRepository.findAll();
     }
 
-    //GET /users
-    @GetMapping("/jpa/users/{id}")
+    @GetMapping("/users/{id}")
     public EntityModel<User> retrieveUsersById(@PathVariable int id) throws UserNotFoundException {
-        Optional<User> userOptional = userRepository.findById(id);
-        if (userOptional.isEmpty()) {
-            throw new UserNotFoundException("id:" + id);
-        }
-        User user = userOptional.get();
+        User user = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("id:" + id));
+
         EntityModel<User> entityModel = EntityModel.of(user);
-        WebMvcLinkBuilder linkToUsers = linkTo(
-                methodOn(this.getClass()).retrieveAllUsers()
-        );
+        WebMvcLinkBuilder linkToUsers = linkTo(methodOn(this.getClass()).retrieveAllUsers());
         entityModel.add(linkToUsers.withRel("all_users"));
         return entityModel;
     }
 
-    /*@GetMapping("/jpa/users/{id}")
-    public EntityModel<User> retrieveUsersByIdorElseThrow(@PathVariable int id) throws UserNotFoundException {
-        User user = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("id:" + id));
-        EntityModel<User> entityModel = EntityModel.of(user);
-        WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveAllUsers());
-        entityModel.add(link.withRel("all_users"));
-        return entityModel;
-    }*/
-
-    //DELETE /user BY ID
-    @DeleteMapping("/jpa/users/{id}")
-    public void deleteUser(@PathVariable int id) throws UserNotFoundException {
-        userRepository.deleteById(id);
-    }
-
-    @PostMapping("/jpa/creatusers")
-    public ResponseEntity<User> creatusers(@Valid @RequestBody User user){
-        userRepository.save(user);
-        return ResponseEntity.created(null).build();
-    }
-
-    @PostMapping("/jpa/userss")
-    public ResponseEntity<User> createUser(@RequestBody User user) {
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
         User savedUser = userRepository.save(user);
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
                 .path("/{id}")
                 .buildAndExpand(savedUser.getId())
                 .toUri();
-        return ResponseEntity.created(location).build();
+        return ResponseEntity.created(location).body(savedUser);
     }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable int id) throws UserNotFoundException {
+        User user = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("id:" + id));
+        userRepository.delete(user);
+        return ResponseEntity.noContent().build();
+    }
+
 //    // POST /users
 //    @PostMapping("/users")
 //    public ResponseEntity<User> createUser(@RequestBody User user) {
